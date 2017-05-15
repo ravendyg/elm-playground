db4166ce8598fae91f7ddc7e73feabf683eb5101
Yaroslav Dashkevich <jdashkevich@xiag.ch>
2017-05-15 15:52:39 +0700

    Experiment with types

---
+++ list.elm
@@ -0,0 +1,50 @@
+import Html exposing (Html, Attribute, div, input, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+
+main =
+  Html.beginnerProgram { model = model, view = view, update = update }
+
+
+-- MODEL
+
+type Lst a = Empty | Node a (Lst a)
+
+type alias Model =
+  { content : Lst String
+  }
+
+model : Model
+model =
+  { content = Node "qwe" (Node "xxv" Empty) }
+
+
+-- UPDATE
+
+type Msg
+  = Change String
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Change newContent ->
+      { model | content = Node newContent model.content}
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div []
+    -- (List.concat [ -- input [ placeholder "Text to add", onInput Change ] []
+        (List.map renderItem [(Node "xxv" Empty)])
+    -- ])
+
+renderItem : Lst String -> Html Msg
+renderItem node =
+    case node of
+        Empty ->
+            div [] [text "end" ]
+        Node st rest ->
+            div [] [ text st ]
