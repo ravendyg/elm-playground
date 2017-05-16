0c9837a8090a07258cb42aa1dfcd564de8eb0c01
Yaroslav Dashkevich <jdashkevich@xiag.ch>
2017-05-16 08:24:12 +0700

    Implement passing timezone from outside

--- time.elm
+++ time.elm
@@ -1,8 +1,11 @@
+port module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing(Time, second)
+
 
 
 main =
@@ -15,30 +18,60 @@ main =
 
 -- MODEL
 
-type alias Model = Time
+type alias Model =
+  { time : Time
+  , offset : Int
+  , zone : String
+  }
 
 init : (Model, Cmd Msg)
 init =
-  (0, Cmd.none)
+  ({time = 0
+  , offset = 0
+  , zone = ""
+  }, Cmd.none)
 
 
 -- UPDATE
 
 type Msg =
   Tick Time
+  | GetTimezone (List String)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Tick newTime ->
-      (newTime, Cmd.none)
+      ({model | time = newTime}, Cmd.none)
+
+    GetTimezone newZone ->
+      let
+        zn =
+          case List.head newZone of
+            Just val -> val
+            Nothing -> "0"
+        offs = case String.toInt (
+          case List.head (List.drop 1 newZone) of
+            Just val -> val
+            Nothing -> "0"
+        ) of
+          Ok val -> val
+          Err msg -> 0
+      in
+        ({model | zone = zn, offset = offs}, Cmd.none)
+
 
 
 -- SUBSCRIPTIONS
 
+port getTimezone : (List String -> msg) -> Sub msg
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Time.every second Tick
+  Sub.batch
+    [ Time.every second Tick
+    , getTimezone GetTimezone
+    ]
 
 
 -- VIEW
@@ -62,18 +95,23 @@ view model =
     ticks = List.range 0 11
 
   in
-    div [ Html.Attributes.style [("textAlign", "center")] ] [
-      svg [ viewBox "0 0 100 100", Svg.Attributes.width "300px" ]
-        (List.concat [
-          [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-          , line [ x1 "50", y1 "50", x2 handSecX, y2 handSecY, stroke "#023963" ] []
-          , line [ x1 "50", y1 "50", x2 handMinX, y2 handMinY, stroke "#523963" ] []
-          , line [ x1 "50", y1 "50", x2 handHouX, y2 handHouY, stroke "#029963" ] []
-          ]
-          , List.map getTick (List.range 0 59)
-        ])
-      , getDigitalClock (getNumericTime model)
-    ]
+    case model.time /= 0 of
+      True ->
+        div [ Html.Attributes.style [("textAlign", "center")] ] [
+          svg [ viewBox "0 0 100 100", Svg.Attributes.width "300px" ]
+            (List.concat [
+              [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
+              , line [ x1 "50", y1 "50", x2 handSecX, y2 handSecY, stroke "#023963" ] []
+              , line [ x1 "50", y1 "50", x2 handMinX, y2 handMinY, stroke "#523963" ] []
+              , line [ x1 "50", y1 "50", x2 handHouX, y2 handHouY, stroke "#029963" ] []
+              ]
+              , List.map getTick (List.range 0 59)
+            ])
+          , getDigitalClock (getNumericTime model)
+        ]
+      False -> div [ Html.Attributes.style [("textAlign", "center")] ] [
+        Html.text "Loading..."
+      ]
 
 -- SVG
 getTick : Int -> Svg Msg
@@ -108,35 +146,34 @@ getAngle units =
   units / 60 * 2 * pi - pi / 2
 
 
-getTspInSeconds : Time -> Int
-getTspInSeconds tsp =
-  round (Time.inSeconds tsp)
+getTspInSeconds : Model -> Int
+getTspInSeconds model =
+  round (Time.inSeconds model.time)
 
-getSeconds : Time -> Float
-getSeconds tsp =
-  toFloat (rem (getTspInSeconds tsp) 60)
+getSeconds : Model -> Float
+getSeconds model =
+  toFloat (rem (getTspInSeconds model) 60)
 
-getMinutes : Time -> Float
-getMinutes tsp =
+getMinutes : Model -> Float
+getMinutes model =
   let
-    sec = (getTspInSeconds tsp)
+    sec = (getTspInSeconds model)
     min = (toFloat sec) / 60
     more = floor ((toFloat sec) / 60 / 60)
   in
     min - (toFloat more) * 60
 
 
-getHours : Time -> Float
-getHours tsp =
+getHours : Model -> Float
+getHours model =
   let
-    sec = (getTspInSeconds tsp)
+    sec = (getTspInSeconds model)
     hou = (toFloat sec) / 60 / 60
     more = floor ((toFloat sec) / 60 / 60 / 24)
     diff = hou - ((toFloat more) * 24)
-    di = if (diff + 7 >= 12) then diff - 12 else diff
+    di = if (diff + (toFloat model.offset) >= 12) then diff - 12 else diff
   in
-    -- +7 is a timezone hack, have found no way to get it programmaticaly
-    (diff + 7) / 12 * 60
+    (diff + (toFloat model.offset)) / 12 * 60
 
 indentTime : Int -> String
 indentTime input =
@@ -147,13 +184,13 @@ indentTime input =
     then "0" ++ str
     else str
 
-getNumericTime : Time -> String
-getNumericTime time =
+getNumericTime : Model -> String
+getNumericTime model =
   let
-    tsp = (getTspInSeconds time)
+    tsp = (getTspInSeconds model)
     sec = rem tsp 60
     min = (tsp % (60 * 60)) // 60
-    hou = (tsp % (60 * 60 * 24)) // (60 * 60) + 7
+    hou = (tsp % (60 * 60 * 24)) // (60 * 60) + model.offset
   in
-    (indentTime hou) ++ ":" ++ (indentTime min) ++ ":" ++ (indentTime sec)
+    (indentTime hou) ++ ":" ++ (indentTime min) ++ ":" ++ (indentTime sec) ++ " (" ++ model.zone ++ ")"
 
